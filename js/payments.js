1542a6c79e3ce0ec7005d9f07649ae2667df72d1
WillieKM <willie@cyber-node.com>
2026-03-08 17:25:27 -0700

    Create payments.js

---
+++ js/payments.js
@@ -0,0 +1,13 @@
+function startCheckout(){
+
+alert("Redirecting to secure payment portal")
+
+}function sendAlert(domain,score){
+
+if(score < 70){
+
+alert("Security risk detected for " + domain)
+
+}
+
+}
